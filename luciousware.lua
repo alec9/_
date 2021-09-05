d0b2d5a0cfcad6a39bc97b804f1d8b9576ecb6ce
i_tapedhim <81587367+alec9@users.noreply.github.com>
2021-09-05 19:48:51 +0300

    Update luciousware.lua

--- luciousware.lua
+++ luciousware.lua
@@ -1,1 +1,8 @@
---script gons be here soon!!!!!!!!!!
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/Matteoislost/Luciousware/main/.lua"), true))()
+if done == false then
+  print("script not done!!")
+  break
+elseif done == true then
+  print("hi hope u have good timre using luci ware!!")
+end
+
